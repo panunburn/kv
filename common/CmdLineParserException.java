2730ceb7483b084ee5e110148ce0f81e88cd02ac
lzheng5 <lingxiaozheng515@gmail.com>
2022-12-01 15:53:22 -0500

    fixed formatting

--- common/CmdLineParserException.java
+++ common/CmdLineParserException.java
@@ -2,14 +2,15 @@ package common;
 
 /**
  * 
- * An exception of this type will be thrown when the CmdLineParser fails to handle an input.
+ * An exception of this type will be thrown when the CmdLineParser fails to
+ * handle an input.
  *
  */
 @SuppressWarnings("serial")
 public class CmdLineParserException extends Exception
 {
-	public CmdLineParserException(String message)
-	{
-		super(message);
-	}
+    public CmdLineParserException(String message)
+    {
+        super(message);
+    }
 }
--- common/EndPoint.java
+++ common/EndPoint.java
@@ -5,52 +5,52 @@ import java.net.InetAddress;
 import java.util.Objects;
 
 /**
- * An endpoint location is of a valid pair IP address and port number. 
+ * An endpoint location is of a valid pair IP address and port number.
  */
 public class EndPoint implements Serializable
 {
-	private static final long serialVersionUID = 1L;
-	private final InetAddress host;
-	private final int port;
-	
-	public EndPoint(InetAddress host, int port)
-	{
-		this.host = host;
-		this.port = port;
-	}
-	
-	public InetAddress getHost()
-	{
-		return host;
-	}
-	
-	public int getPort()
-	{
-		return port;
-	}
-	
-	@Override
-	public String toString()
-	{
-		return host + Config.defaultAddrDelim() + port;
-	}
-	
-	@Override
-	public int hashCode()
-	{
-		return Objects.hash(host, port);
-	}
+    private static final long serialVersionUID = 1L;
+    private final InetAddress host;
+    private final int port;
 
-	@Override
-	public boolean equals(Object obj)
-	{
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		EndPoint other = (EndPoint) obj;
-		return Objects.equals(host, other.host) && port == other.port;
-	}
+    public EndPoint(InetAddress host, int port)
+    {
+        this.host = host;
+        this.port = port;
+    }
+
+    public InetAddress getHost()
+    {
+        return host;
+    }
+
+    public int getPort()
+    {
+        return port;
+    }
+
+    @Override
+    public String toString()
+    {
+        return host + Config.defaultAddrDelim() + port;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(host, port);
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        EndPoint other = (EndPoint) obj;
+        return Objects.equals(host, other.host) && port == other.port;
+    }
 }
